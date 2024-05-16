361b7f739488f782cc31768bf4fa61e79e281183
maranix <ramanverma4183@gmail.com>
2024-05-16 20:04:48 +0530

    feat: show error level logs by default

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"fmt"
 	"log/slog"
 	"os"
 
@@ -11,33 +10,43 @@ import (
 
 const ReleaseVersion = "0.0.1-alpha"
 
+var verbose bool
 var rootCmd = &cobra.Command{
 	Use:   "monitor",
 	Short: "Monitor restart command/service on filesystem changes effortlessly",
 	Long: `Monitor is a cli-tool built for development workflows where changes 
 in configuration files or code requires restarting a service.`,
-	Args: cobra.MinimumNArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-		pathArg := args[0]
-		cmdArg := args[1]
+	Args: cobra.MinimumNArgs(2),
+	Run:  handleRun,
+}
 
-		slog.Info(fmt.Sprintf("pathArg: %s, cmd: %s", pathArg, cmdArg))
+func handleRun(cmd *cobra.Command, args []string) {
+	if !verbose {
+		slog.SetLogLoggerLevel(slog.LevelError)
+	}
 
-		obs, err := observe.Create(pathArg, cmdArg)
-		if err != nil {
-			slog.Error("Failed to create an observable", err)
-		}
+	pathArg := args[0]
+	cmdArg := args[1]
 
-		obs.Observe()
-	},
+	obs, err := observe.Create(pathArg, cmdArg)
+	if err != nil {
+		slog.Error("Failed to create an observable", err)
+	}
+
+	obs.Observe()
 }
 
 func addCommands() {
 	rootCmd.AddCommand(versionCmd)
 }
 
+func addFlags() {
+	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "output additional information")
+}
+
 func Execute() {
 	addCommands()
+	addFlags()
 
 	if err := rootCmd.Execute(); err != nil {
 		slog.Error(err.Error())
