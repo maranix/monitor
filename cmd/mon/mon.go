04754654eced0c19ec24af39055dfcd2bbf7c9f6
maranix <ramanverma4183@gmail.com>
2024-06-14 23:52:31 +0530

    fix ci and move cli from pkg to internal

--- cmd/mon/mon.go
+++ cmd/mon/mon.go
@@ -1,18 +1,18 @@
-package mon
+package main
 
 import (
 	"context"
 	"log/slog"
 	"os"
 
-	"github.com/maranix/monitor/pkg/cli"
+	"github.com/maranix/monitor/internal/cli"
 )
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	if err := cli.ExecuteContext(ctx); err != nil {
+	if err := cli.ExecuteWithContext(ctx); err != nil {
 		slog.Error(err.Error())
 		os.Exit(1)
 	}
